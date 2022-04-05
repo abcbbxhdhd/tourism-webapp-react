35391f5b94452dc36babd72bec769ad403131d46
abcbbxhdhd <ajax76@protonmail.com>
2022-04-05 18:42:04 +0300

    [T2] Setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import React from "react"
+import { Routes, Route } from "react-router-dom"
+import AdminRequestsPage from "./pages/AdminRequestsPage/AdminRequestsPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import PostDetailPage from "./pages/PostDetailPage/PostDetailPage";
+import PostsPage from "./pages/PostsPage/PostsPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import RequestFormPage from "./pages/RequestFormPage/RequestFormPage";
 
 function App() {
   return (
-    <h1>App</h1>
+    <Routes>
+      // AuthMiddleware that will redirect to posts or login 
+      <Route path="/" element={<h1>/</h1>} />
+      
+      <Route path="/posts" element={<PostsPage />} />
+      <Route path="/posts/:postId" element={<PostDetailPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/profile/:profileId" element={<ProfilePage />} />
+      <Route path="/requests" element={<AdminRequestsPage />} />
+      <Route path="/requestPost" element={<RequestFormPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<RegisterPage />} />
+    </Routes>
   );
 }
 
